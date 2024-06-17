a21dad62b9a6edf33b06960e080c9a9bda77129f
Sanjay <crazysanjay000@gmail.com>
2024-06-17 09:41:30 +0530

    final commit ready

--- weather.py
+++ weather.py
@@ -2,8 +2,6 @@ from tkinter import *
 from tkinter import ttk
 import requests
 
-
-
 def data_get():
     city=city_name.get()
     data = requests.get("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=5fff569985da80778dbac0c62e5e18b3").json()
@@ -12,10 +10,8 @@ def data_get():
     temp_label1.config(text=str(int(data["main"]["temp"]-273.15)))
     per_label1.config(text=data["main"]["pressure"])
 
-
-
 win = Tk()
-win.title("Sanjay Kumar Chaurasiya")
+win.title("Sanjay")
 win.config(bg = "blue")
 win.geometry("500x570")
 
@@ -24,14 +20,11 @@ name_label = Label(win,text="Weather APP",
 name_label.place(x=25,y=50,height=50,width=450)
 
 
-
 city_name=StringVar()
 list_name = ["Andhra Pradesh","Arunachal Pradesh ","Assam","Bihar","Chhattisgarh","Goa","Gujarat","Haryana","Himachal Pradesh","Jammu and Kashmir","Jharkhand","Karnataka","Kerala","Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand","West Bengal","Andaman and Nicobar Islands","Chandigarh","Dadra and Nagar Haveli","Daman and Diu","Lakshadweep","National Capital Territory of Delhi","Puducherry"]
 com = ttk.Combobox(win,text="Weather APP", values=list_name,
                    font=("Times New Roman" ,20, "bold"),textvariable=city_name)
 com.place(x=25,y=120,height=50,width=450)
-
-
 
 
 w_label = Label(win,text="Weather Climate",
@@ -43,8 +36,6 @@ w_label1 = Label(win,text="",
 w_label1.place(x=250,y=260,height=50,width=210)
 
 
-
-
 wb_label = Label(win,text="Weather description",
                    font=("Times New Roman" ,17))
 wb_label.place(x=25,y=330,height=50,width=210)
@@ -52,7 +43,6 @@ wb_label.place(x=25,y=330,height=50,width=210)
 wb_label1 = Label(win,text="",
                    font=("Times New Roman" ,17))
 wb_label1.place(x=250,y=330,height=50,width=210)
-
 
 
 temp_label = Label(win,text="temperature",
@@ -64,8 +54,6 @@ temp_label1 = Label(win,text="",
 temp_label1.place(x=250,y=400,height=50,width=210)
 
 
-
-
 per_label = Label(win,text="pressure",
                    font=("Times New Roman" ,20))
 per_label.place(x=25,y=470,height=50,width=210)
@@ -73,7 +61,6 @@ per_label.place(x=25,y=470,height=50,width=210)
 per_label1 = Label(win,text="",
                    font=("Times New Roman" ,20))
 per_label1.place(x=250,y=470,height=50,width=210)
-
 
 
 done_button= Button(win,text="Done",
